132df6f36451467d266f23d18a9010aaad2bf86a
unknown <andrycoding@gmail.com>
2022-12-11 01:56:44 +0700

    Add codeblock background

--- client/src/components/RichTextEditor.jsx
+++ client/src/components/RichTextEditor.jsx
@@ -1,32 +1,41 @@
-import { useEffect, useState } from "react";
+import { useState } from "react";
 import ReactQuill from "react-quill";
 import "react-quill/dist/quill.snow.css";
 import { useColorModeContext } from "../context/ColorModeContext";
 
 export default function RichTextEditor({ content, setContent }) {
   const { colorMode } = useColorModeContext();
-  const modules = [
-    ["bold", "italic", "underline", "strike"],
-    ["link"],
-    ["blockquote", "code-block"],
-    [{ list: "ordered" }, { list: "bullet" }],
-    [{ script: "sub" }, { script: "super" }],
-    ["align", { align: "center" }, { align: "right" }, { align: "justify" }],
-  ];
   return (
     <ReactQuill
       id="editor"
-      className="quill"
       style={{
+        background: colorMode === "light" ? "#faf7f7" : "#90caf9",
         borderRadius: "5px",
-        background: colorMode === "light" ? "#faf7f7" : "#1e1e1e",
-        color: colorMode === "light" ? "black" : "white",
-        fontSize: "2rem",
       }}
       theme="snow"
       value={content}
       onChange={() => {
         setContent(document.getElementsByClassName("ql-editor")[0].innerHTML);
+      }}
+      formats={[
+        "header",
+        "size",
+        "bold",
+        "italic",
+        "underline",
+        "strike",
+        "blockquote",
+        "link",
+        "code-block",
+      ]}
+      modules={{
+        toolbar: [
+          [{ header: [1, 2, 3, 4, 5, 6] }],
+          [{ size: [] }],
+          ["bold", "italic", "underline", "strike", "blockquote"],
+          [{ list: "ordered" }, { list: "bullet" }]["link"],
+          ["code-block"],
+        ],
       }}
     />
   );
